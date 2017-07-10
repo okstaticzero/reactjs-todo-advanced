81824b39734e57ee06a8644c74723efa605a2757
Matt Wallace <mwallace@zerostatic.com>
2017-07-10 12:44:01 -0600

    remove all

--- src/reducers/Index.js
+++
@@ -1,8 +0,0 @@
-import CategoryTree from './CategoryTree';
-import { combineReducers } from 'redux';
-import undoable from 'redux-undo';
-
-export default combineReducers({
-  categories:undoable(CategoryTree)
-  //categories:CategoryTree
-})
